4452b60dce21785f7e1fa132f0055124b5ed6caf
mm1973ro <mm1973ro@gmail.com>
2015-06-16 23:26:32 +0300

    meta seo

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,6 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="keywords" content="cavaleri, templieri, jacques, molay, timisoara, osmth, ordinul, militar, comanderia, templul, ierusalim"/>
 	<title>Comanderia Jaques de Molay - Timisoara</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8" />	
 </head>
